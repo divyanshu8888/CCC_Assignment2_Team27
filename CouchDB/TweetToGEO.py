35600497c0f522817c30c60208723a6f4663606a
Pagnarith-Pit <111168396+Pagnarith-Pit@users.noreply.github.com>
2023-05-19 16:25:36 +1000

    Updated Script (full-working version)

--- CouchDB/TweetToGEO.py
+++ CouchDB/TweetToGEO.py
@@ -1,8 +1,8 @@
 import json
 
-# The files extracted from the views are to be called 'results.json'
-tweetFile = open("results.json")
-tweets_collection = json.load(tweetFile)
+# The files extracted from the views are to be called 'xx.json'. Replace the name here.
+tweetFile = open("test.json")
+tweets_collection = json.load(tweetFile)["rows"]
 
 geojson = {  
     "type":"FeatureCollection",
@@ -10,6 +10,7 @@ geojson = {
     }
 
 for tweet in tweets_collection:
+    tweet = tweet["value"]
     tweets_box = tweet["bbox"]
     long = (tweets_box[0] + tweets_box[2])/2
     lat = (tweets_box[1] + tweets_box[3])/2
@@ -24,5 +25,8 @@ for tweet in tweets_collection:
     geojson["features"].append(feature)
 
 # File output will have the name spatial_data.geojson
-    with open('spatial_data.geojson', 'w') as fp:
+    with open('spatial_data_medical.geojson', 'w') as fp:
         json.dump(geojson, fp)
+
+
+# curl -XGET "http://admin:admin@172.26.135.230:5984/twitter/_design/travelhistory/_view/travel-view?reduce=false&include_docs=false" > /data/travel.json
